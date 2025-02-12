16c5dbad91a9221d7e446bd87d1aa0043a64da39
paras <doyeparas196@gmail.com>
2025-02-12 16:55:03 +0530

    new

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import About from './About.jsx'
 import Nav from './Nav.jsx'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Nav />
     <br />
     <About/>
+    <br />
   </StrictMode>
 )
